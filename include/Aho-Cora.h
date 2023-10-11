e5e090c0c9133f994f60f68bc0a8b67b4c5036fa
renauga <talerasaumya@yahoo.in>
2023-10-11 13:13:13 +0530

    add Aho-Corasick automaton

---
+++ include/Aho-Cora.h
@@ -0,0 +1,33 @@
+#ifndef AHOCORA
+#define AHOCORA
+
+#include <bits/stdc++.h>
+
+const int alphabet = 26;
+
+struct AhoNode {
+    int next[alphabet]; // direct transition from this node using a character
+    bool output = false; // is this node an exact match
+    int p = -1; // index of parent
+    char pch; // character of parent edge
+    int link = -1; // index of suffix link
+    int go[alphabet]; // transition from this node
+
+    AhoNode(int p, char ch);
+};
+
+struct AhoCorasick {
+    std::vector<AhoNode> t; // the nodes in the automaton
+
+    AhoCorasick();
+
+    void add_string(std::string const& s);
+
+    int go(int v, char ch);
+
+    int get_link(int v);
+};
+
+
+#endif
+ 
---
+++ tests/testAho-Cora.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+
+#include "../include/trace.h"
+#include "../include/Aho-Cora.h"
+
+int main() {
+    vector<string> s = {"a", "ab", "bc", "bca", "c", "caa"};
+    AhoCorasick aut;
+    for(int i=0;i<s.size();i++) {
+        aut.add_string(s[i]);
+    }
+    assert(aut.t.size() == 9);
+    trace(aut.t.size());
+    return 0;
+}
